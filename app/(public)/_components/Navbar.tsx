ce854079ee13113faf0d2a60200cfd6368d97160
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-07-09 11:11:44 +0100

    add landing page and customize shadcn design using tweakcn

---
+++ app/(public)/_components/Navbar.tsx
@@ -0,0 +1,69 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Logo from "@/public/logo1.png";
+import { ThemeToggle } from "@/components/ui/themeToggle";
+import { authClient } from "@/lib/auth-client";
+import { buttonVariants } from "@/components/ui/button";
+import UserDropdown from "./UserDropdown";
+
+const navigationItems = [
+  { name: "Home", href: "/" },
+  { name: "Courses", href: "/courses" },
+  { name: "Dashboard", href: "/dashboard" },
+];
+
+const Navbar = () => {
+  const { data: session, isPending } = authClient.useSession();
+
+  return (
+    <header
+      className='sticky top-0 z-50 w-full border-b bg-background/95 
+    backdrop-blur-[backdrop-filter]:bg-background/60'>
+      <div className='container flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8'>
+        <Link href='/' className='flex items-center space-x-2 mr-4'>
+          <Image src={Logo} alt='Logo' className='size-9' />
+          <span className='font-bold'>MM-LMS®</span>
+        </Link>
+        {/* Desktop navigation */}
+        <nav className='hidden md:flex md:flex-1 md:items-center md:justify-between'>
+          <div className='flex items-center space-x-2'>
+            {navigationItems.map(({ name, href }) => (
+              <Link
+                key={name}
+                href={href}
+                className='text-sm font-medium transition-colors 
+              hover:text-primary'>
+                {name}
+              </Link>
+            ))}
+          </div>
+
+          <div className='flex items-center space-x-4'>
+            <ThemeToggle />
+
+            {isPending ? null : session ? (
+              <UserDropdown name={session.user.name} email={session.user.email} image={session.user.image || ""} />
+            ) : (
+              <>
+                <Link
+                  href='/login'
+                  className={buttonVariants({
+                    variant: "secondary",
+                  })}>
+                  Login
+                </Link>
+                <Link href='/login' className={buttonVariants()}>
+                  Get started
+                </Link>
+              </>
+            )}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
